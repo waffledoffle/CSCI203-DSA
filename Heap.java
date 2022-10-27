b9298eb08dcd1834515e47ec4d35aea2f9cf7819
waffledoffle <jackwaffledoffle@gmail.com>
2022-10-27 20:22:27 +1100

    adding to heap.java

--- Heap.java
+++ Heap.java
@@ -1,10 +1,10 @@
 public class Heap {
     //static final int HEAP_SIZE = 20;
-    static int[] heap = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, -1, -2, -3, -4};
-    static int size = 20;
+    static int[] heap = {5, 14, 23, 32, 41, 87, 90, 50, 64, 53, 43};
+    static int size = heap.length;
 
     public static void main(String[] args) {
-        makeHeap();
+        add(16);
         for (int i = 0; i < size; i++) {
             System.out.println(heap[i]);
         }
@@ -21,17 +21,26 @@ public class Heap {
         if (child >= size) {
             return;
         }
-        if ((child + 1 < size) && (heap[child] < heap[child + 1])) {
+        if ((child + 1 < size) && (heap[child] > heap[child + 1])) {
+            System.out.println("Comparing child: " + heap[child] + " With child + 1: " + heap[child + 1]);
             child = child + 1;
         }
-        if (heap[current] < heap[child]) {
+        System.out.println("Comparing current: " + heap[current] + " With child: " + heap[child]);
+        if (heap[current] > heap[child]) {
             swap(current, child);
             siftDown(child);
         }
     }
 
-    public static void add(int new) {
-        
+    public static void add(int added) {
+        int[] temp = new int[heap.length + 1];
+        for (int i = 0; i < heap.length; i++) {
+            temp[i] = heap[i];
+        } 
+        temp[heap.length] = added;
+        size = size + 1;
+        siftUp(size - 1);
+
     }
 
     public static void remove() {
@@ -59,6 +68,7 @@ public class Heap {
         }
         int parent = (current - 1) / 2;
 
+        System.out.println("Comparing current: " + heap[current] + " With parent: " + heap[parent]);
         if (heap[current] < heap[parent]) {
             swap(current, parent);
             siftUp(parent);
